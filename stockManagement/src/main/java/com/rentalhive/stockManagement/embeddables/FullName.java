e61c39ba7a988cae41c992114f529d6778e08067
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-11-13 19:09:56 +0100

    Create User Entity and Full Name embeddable class

---
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/embeddables/FullName.java
@@ -0,0 +1,55 @@
+package com.rentalhive.stockManagement.embeddables;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Embeddable
+@Getter
+@Setter
+public class FullName {
+
+    @NotEmpty(message = "The first name can not be empty")
+    @NotNull(message = "The first name can not be null")
+    @NotBlank(message = "The first name can not be blank")
+    @Pattern(regexp = "^[^\\s]*$", message = "No space allowed in first name")
+    private String firstName;
+
+    @NotBlank(message = "The middle name can not be blank")
+    @Pattern(regexp = "^[^\\s]*$", message = "No Space Allowed in middle name")
+    private String middleName;
+
+    @NotEmpty(message = "The last name can not be empty")
+    @NotNull(message = "The last name can not be null")
+    @NotBlank(message = "The last name can not be blank")
+    @Pattern(regexp = "^[^\\s]*$", message = "No Space Allowed in last name")
+    private String lastName;
+
+    public FullName() {
+    }
+
+    public FullName(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public FullName(String firstName, String middleName, String lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                " firstName='" + getFirstName() + "'" +
+                ", middleName='" + getMiddleName() + "'" +
+                ", lastName='" + getLastName() + "'" +
+                "}";
+    }
+}
---
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/entities/User.java
@@ -0,0 +1,52 @@
+package com.rentalhive.stockManagement.entities;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.rentalhive.stockManagement.embeddables.FullName;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Embedded
+    @Valid
+    private FullName full_name;
+
+    @NotEmpty(message = "The user name can not be empty")
+    @NotNull(message = "The user name can not be null")
+    @NotBlank(message = "The user name can not be blank")
+    @Pattern(regexp = "^[^\\s]*$", message = "No space allowed in user name")
+    private String user_name;
+
+    @Embedded
+    @Valid
+    private AddressEmail email;
+
+    @Embedded
+    @Valid
+    private Password password;
+
+    @NotNull(message = "The date of creation of account can npt be null")
+    private LocalDateTime date_creation_account;
+}
