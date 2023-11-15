598735b9972c02522bb786b89366c2ed2f79440f
khaouti nouhaila <khaoutinouhaila@gmail.com>
2023-11-15 16:01:51 +0100

    almost done with the add stock testing

--- stockManagement/src/main/java/com/rentalhive/stockManagement/repositories/StockRepository.java
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/repositories/StockRepository.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.rentalhive.stockManagement.entities.Stock;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    Optional<Stock> findByRegistrationNumber(String registrationNumber);
 }
--- stockManagement/src/test/java/com/rentalhive/stockManagement/services/impls/StockServiceTest.java
+++ stockManagement/src/test/java/com/rentalhive/stockManagement/services/impls/StockServiceTest.java
@@ -4,17 +4,29 @@ import com.rentalhive.stockManagement.embeddables.AddressEmail;
 import com.rentalhive.stockManagement.embeddables.FullName;
 import com.rentalhive.stockManagement.embeddables.Password;
 import com.rentalhive.stockManagement.entities.*;
+import com.rentalhive.stockManagement.exceptions.costums.AlreadyExistsException;
+import com.rentalhive.stockManagement.exceptions.costums.DoNotExistsException;
+import com.rentalhive.stockManagement.exceptions.costums.ValidationException;
 import com.rentalhive.stockManagement.repositories.StockRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.lang.reflect.Field;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class StockServiceTest {
     private StockServiceImp stockServiceImp;
     private StatusServiceImp statusServiceImp;
@@ -30,16 +42,21 @@ class StockServiceTest {
         equipmentServiceImp = mock(EquipmentServiceImp.class);
         stockServiceImp = new StockServiceImp(stockRepository);
     }
+
     private Stock createValidStock() {
         Stock stock = new Stock();
         stock.setId(1L);
         stock.setAdded_by(createValidUser());
         stock.setStatus(createValidStatus());
         stock.setEquipment(createValidEquipment());
+        stock.setRegistrationNumber("3456GTJK");
         return stock;
     }
+
     private User createValidUser() {
         User user = new User();
+        user.setId(1L);
+        user.setUser_name("baouboula");
         AddressEmail addressEmail=new AddressEmail("nouhaila@gmail.com");
         user.setEmail(addressEmail);
         Password password=new Password("nouha");
@@ -69,9 +86,9 @@ class StockServiceTest {
         Status status = createValidStatus();
         Equipment equipment = createValidEquipment();
         User user=createValidUser();
-        when(userServiceImp.findById(1L)).thenReturn(Optional.of(user));
-        when(statusServiceImp.findById(1L)).thenReturn(Optional.of(status));
-        when(equipmentServiceImp.findById(1L)).thenReturn(Optional.of(equipment));
+        when(userServiceImp.findById(anyLong())).thenReturn(Optional.of(user));
+        when(statusServiceImp.findById(anyLong())).thenReturn(Optional.of(status));
+        when(equipmentServiceImp.findById(anyLong())).thenReturn(Optional.of(equipment));
         when(stockRepository.save(stock)).thenReturn(stock);
 
         Stock stockRes = stockServiceImp.addStock(stock);
@@ -81,18 +98,26 @@ class StockServiceTest {
     @Test
     void testAddStockWhenUserNotFoundThenThrowNotFoundException(){
         Stock stock = createValidStock();
-        when(userServiceImp.findById(1L)).thenReturn(Optional.empty());
-        assertThrows(UserNotFoundException.class,stockServiceImp.addStock(stock));
+        when(userServiceImp.findById(anyLong())).thenReturn(Optional.empty());
+        assertThrows(DoNotExistsException.class, ()->stockServiceImp.addStock(stock),"user does not exist");
+    }
+
+    @Test
+    void testAddStockWhenStockAlreadyExistThenThrowAlreadyExistException(){
+        Stock stock = createValidStock();
+        when(stockRepository.findByRegistrationNumber(anyString())).thenReturn(Optional.of(stock));
+        assertThrows(AlreadyExistsException.class, ()->stockServiceImp.addStock(stock),"stock with the same registration name already exist");
     }
 
     @Test
     void testAddStockWhenEquipmentNotFoundThenThrowNotFoundException(){
         Stock stock = createValidStock();
         User user=createValidUser();
-        when(userServiceImp.findById(1L)).thenReturn(Optional.of(user));
-        when(equipmentServiceImp.findById(1L)).thenReturn(Optional.empty());
-        assertThrows(EquipmentNotFoundException.class,stockServiceImp.addStock(stock));
+        when(userServiceImp.findById(anyLong())).thenReturn(Optional.of(user));
+        when(equipmentServiceImp.findById(anyLong())).thenReturn(Optional.empty());
+        assertThrows(DoNotExistsException.class, ()-> stockServiceImp.addStock(stock),"the equipment does not exist");
     }
+
     @Test
     void testAddStockWhenCategoryNotFoundThenThrowNotFoundException(){
         Stock stock = createValidStock();
@@ -101,21 +126,46 @@ class StockServiceTest {
         when(userServiceImp.findById(1L)).thenReturn(Optional.of(user));
         when(equipmentServiceImp.findById(1L)).thenReturn(Optional.of(equipment));
         when(statusServiceImp.findById(1L)).thenReturn(Optional.empty());
-        assertThrows(StatusNotFoundException.class,stockServiceImp.addStock(stock));
+        assertThrows(DoNotExistsException.class, ()->stockServiceImp.addStock(stock),"category does not exist");
     }
 
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void testStockAttributesForNull(Stock stock, String nullField) throws IllegalAccessException {
+        Field[] fields = stock.getClass().getDeclaredFields();
 
+        for (Field field : fields) {
+            field.setAccessible(true);
+                if (field.getName().equals(nullField)) {
+                    field.set(stock, null);
+                }
+        }
+        assertThrows(ValidationException.class, () -> stockServiceImp.addStock(stock),"one of this stock fields is null");
 
-    @Test
-    public void testSaveStockWhenNullStockThenThrowException() {
-        assertThrows(NullPointerException.class, (Executable) stockServiceImp.addStock(null));
+    }
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void testStockAttributesAreBlank(Stock stock, String nullField) throws IllegalAccessException {
+        Field[] fields = stock.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            field.setAccessible(true);
+            if (field.getName().equals(nullField)) {
+                field.set(stock, "");
+            }
+        }
+        assertThrows(ValidationException.class, () -> stockServiceImp.addStock(stock),"one of this stock fields is blank");
+
     }
 
-    @Test
-    void updateStock() {
+    private  Stream<Arguments> provideTestData() {
+        Stock stock = createValidStock();
+        return Stream.of(
+                Arguments.of(stock, "name"),
+                Arguments.of(stock, "price"),
+                Arguments.of(stock, "user"),
+                Arguments.of(stock, "equipment")
+        );
     }
 
-    @Test
-    void deleteStock() {
-    }
 }
