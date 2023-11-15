18c70b9aaf48fd2987e2a1beedb1ff2e5dbb24a3
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-11-15 13:30:50 +0100

    Add Test for null values for addDemand (DemandeService)

--- stockManagement/src/test/java/com/rentalhive/stockManagement/services/impls/DemandeServiceImpTest.java
+++ stockManagement/src/test/java/com/rentalhive/stockManagement/services/impls/DemandeServiceImpTest.java
@@ -2,11 +2,14 @@ package com.rentalhive.stockManagement.services.impls;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Nested;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -14,11 +17,15 @@ import static org.mockito.Mockito.*;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 import javax.validation.constraints.NotNull;
 
 import com.rentalhive.stockManagement.entities.Demande;
 import com.rentalhive.stockManagement.entities.User;
+import com.rentalhive.stockManagement.exceptions.costums.ValidationException;
 import com.rentalhive.stockManagement.repositories.DemandeRepository;
 
 import liquibase.repackaged.org.apache.commons.lang3.ObjectUtils.Null;
@@ -40,66 +47,155 @@ public class DemandeServiceImpTest {
 
     public DemandeServiceImp demandeServiceImp;
 
-    @BeforeEach
-    public void setUpRepositoryAndServiceOfDemande() {
-        demandeRepository = mock(DemandeRepository.class);
-        demandeServiceImp = new DemandeServiceImp(demandeRepository);
-    }
-
     @Nested
     class addDemande {
 
+        User renter = new User();
+
+        User verified_by = null;
+
+        String description = "Hello World !!";
+
+        Boolean accepted = false;
+
+        LocalDateTime date_verification = getDateTime(null);
+
+        LocalDateTime date_reservation = getDateTime("2004-01-20T12:00:00");
+
+        LocalDateTime date_expiration = getDateTime("2005-01-20T12:00:00");
+
+        LocalDateTime date_demande = getDateTime("2004-01-01T12:00:00");
+
+        @BeforeEach
+        public void setUpRepositoryAndServiceOfDemande() {
+            demandeRepository = mock(DemandeRepository.class);
+            demandeServiceImp = new DemandeServiceImp(demandeRepository);
+            resetTheAttributes();
+        }
+
         @Test
-        public void whenDateVerificationIsNull_thenShouldNotThrowIllegalArgument() {
-
-            User renter = new User();
-
-            User verified_by = mock(User.class);
-
-            String description = "Hello World !!";
-
-            Boolean accepted = false;
-
-            LocalDateTime date_verification = getDateTime(null);
-
-            LocalDateTime date_reservation = getDateTime("2004-01-20T12:00:00");
-
-            LocalDateTime date_expiration = getDateTime("2005-01-20T12:00:00");
-
-            LocalDateTime date_demande = getDateTime("2004-01-01T12:00:00");
-
-            Demande demande = new Demande(renter, description, accepted, verified_by, date_verification,
-                    date_reservation,
-                    date_expiration, date_demande);
-
+        public void whenAllIsGood_thenShouldNotThrowIllegalArgument() {
             assertDoesNotThrow(() -> {
-                demandeServiceImp.addDemande(demande);
+                demandeServiceImp.addDemande(createDemande());
             });
         }
 
-        public void whenDateDescriptionIsNull_thenShouldThrowIllegalArgument() {
+        @TestFactory
+        public Stream<DynamicTest> whenAddingADemandeThatIsVerifiedFromTheBegining_thenShouldThrowValidationException() {
 
-            User renter = new User();
+            List<Demande> demands = new ArrayList<>();
 
-            User verified_by = mock(User.class);
+            verified_by = new User();
+            date_verification = getDateTime("2004-01-01T12:00:00");
+            accepted = true;
 
-            String description = null;
+            demands.add(createDemande());
 
-            Boolean accepted = false;
+            verified_by = null;
+            date_verification = getDateTime("2004-01-01T12:00:00");
+            accepted = false;
 
-            LocalDateTime date_verification = getDateTime(null);
+            demands.add(createDemande());
 
-            LocalDateTime date_reservation = getDateTime("2004-01-20T12:00:00");
+            verified_by = new User();
+            date_verification = null;
+            accepted = true;
 
-            LocalDateTime date_expiration = getDateTime("2005-01-20T12:00:00");
+            demands.add(createDemande());
 
-            LocalDateTime date_demande = getDateTime("2004-01-01T12:00:00");
+            return demands.stream().map(demand -> DynamicTest.dynamicTest(demand.toString(), () -> {
+                assertThrows(ValidationException.class, () -> {
+                    demandeServiceImp.addDemande(demand);
+                });
+            }));
 
-            Demande demande = new Demande(renter, description, accepted, verified_by, date_verification,
-                    date_reservation,
+        }
+
+        @TestFactory
+        public Stream<DynamicTest> whenSomeThingWrongWithTheDates_thenShouldThrowIllegalArgument() {
+
+            List<Demande> demands = new ArrayList<>();
+
+            // Reservation Is Higher Than Date Expiration
+            date_expiration = getDateTime("2004-01-20T12:00:00");
+            date_reservation = getDateTime("2005-01-20T12:00:00");
+            demands.add(createDemande());
+
+            // Date Demande Is Higher Than Date Reservation
+            date_reservation = getDateTime("2004-01-20T12:00:00");
+            date_expiration = getDateTime("2005-01-20T12:00:00");
+            date_demande = getDateTime("2006-01-20T12:00:00");
+            demands.add(createDemande());
+
+            // Date Demande Is Between Date Reservation And Date Expiration
+            date_reservation = getDateTime("2004-01-20T12:00:00");
+            date_demande = getDateTime("2005-01-20T12:00:00");
+            date_expiration = getDateTime("2006-01-20T12:00:00");
+            demands.add(createDemande());
+
+            // Date verification is higher than date reservation
+            this.date_reservation = getDateTime("2004-01-20T12:00:00");
+            this.date_expiration = getDateTime("2005-01-20T12:00:00");
+            this.date_demande = getDateTime("2004-01-01T12:00:00");
+            this.date_verification = getDateTime("2006-01-20T12:00:00");
+            demands.add(createDemande());
+
+            return demands.stream().map(demand -> DynamicTest.dynamicTest(demand.toString(), () -> {
+                assertThrows(ValidationException.class, () -> {
+                    demandeServiceImp.addDemande(demand);
+                });
+            }));
+        }
+
+        @TestFactory
+        public Stream<DynamicTest> whenNonNullableAttributeAreNull_thenShouldThrowValidationException() {
+
+            List<Demande> demands = new ArrayList<>();
+
+            demands.add(null);
+
+            description = null;
+            demands.add(createDemande());
+            resetTheAttributes();
+
+            renter = null;
+            demands.add(createDemande());
+            resetTheAttributes();
+
+            date_expiration = null;
+            demands.add(createDemande());
+            resetTheAttributes();
+
+            date_reservation = null;
+            demands.add(createDemande());
+            resetTheAttributes();
+
+            date_demande = null;
+            demands.add(createDemande());
+            resetTheAttributes();
+
+            return demands.stream().map(demand -> DynamicTest.dynamicTest(demand.toString(), () -> {
+                assertThrows(ValidationException.class, () -> {
+                    demandeServiceImp.addDemande(demand);
+                });
+            }));
+
+        }
+
+        public void resetTheAttributes() {
+            this.renter = new User();
+            this.verified_by = null;
+            this.description = "Hello World !!";
+            this.accepted = false;
+            this.date_verification = getDateTime(null);
+            this.date_reservation = getDateTime("2004-01-20T12:00:00");
+            this.date_expiration = getDateTime("2005-01-20T12:00:00");
+            this.date_demande = getDateTime("2004-01-01T12:00:00");
+        }
+
+        public Demande createDemande() {
+            return new Demande(renter, description, accepted, verified_by, date_verification, date_reservation,
                     date_expiration, date_demande);
-
-           
         }
     }
 
