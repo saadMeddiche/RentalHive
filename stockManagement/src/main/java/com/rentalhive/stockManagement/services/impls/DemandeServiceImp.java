4e75abff83b29ed859880649e83380a5da83bb9c
khaouti nouhaila <khaoutinouhaila@gmail.com>
2023-11-16 15:13:31 +0100

    merge nouhaila's branch with the main branch

--- stockManagement/src/main/java/com/rentalhive/stockManagement/services/impls/DemandeServiceImp.java
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/services/impls/DemandeServiceImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.rentalhive.stockManagement.entities.Demande;
 import com.rentalhive.stockManagement.exceptions.costums.ValidationException;
 import com.rentalhive.stockManagement.repositories.DemandeRepository;
+import com.rentalhive.stockManagement.repositories.UserRepository;
 import com.rentalhive.stockManagement.services.DemandeService;
 
 public class DemandeServiceImp implements DemandeService {
@@ -12,9 +13,9 @@ public class DemandeServiceImp implements DemandeService {
     private DemandeRepository demandeRepository;
 
     public DemandeServiceImp(DemandeRepository demandeRepository) {
+
         this.demandeRepository = demandeRepository;
     }
-
     @Override
     public List<Demande> getAllDemandes() {
         return null;
@@ -22,7 +23,7 @@ public class DemandeServiceImp implements DemandeService {
 
     @Override
     public Demande addDemande(Demande demande) {
-        return demande;
+            return demande;
     }
 
     @Override
