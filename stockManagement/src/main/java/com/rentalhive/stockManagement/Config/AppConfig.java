48ab3f78a0556fd4c31cbd80e46641fd5dfdc9ed
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-11-22 11:46:40 +0100

    replace setter dependencie injection with constructure injection

---
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/Config/AppConfig.java
@@ -0,0 +1,11 @@
+package com.rentalhive.stockManagement.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+
+}
---
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/converters/EquipmentConverter.java
@@ -0,0 +1,89 @@
+package com.rentalhive.stockManagement.converters;
+
+import com.rentalhive.stockManagement.entities.Equipment;
+
+import com.rentalhive.stockManagement.entities.User;
+import com.rentalhive.stockManagement.services.CategoryService;
+import com.rentalhive.stockManagement.services.UserService;
+
+import com.rentalhive.stockManagement.services.EquipmentService;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.rentalhive.stockManagement.dto.equipmentDtos.EquipmentAddDto;
+import com.rentalhive.stockManagement.dto.equipmentDtos.EquipmentUpdateDto;
+
+import com.rentalhive.stockManagement.services.impls.UserServiceImp;
+
+import com.rentalhive.stockManagement.entities.Category;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EquipmentConverter {
+
+
+
+    public final UserService userservice;
+
+    public final CategoryService categoryService;
+
+    public final EquipmentService equipmentService;
+
+    public EquipmentConverter(UserService userService, CategoryService categoryService, EquipmentService equipmentService) {
+        this.userservice = userService;
+        this.categoryService = categoryService;
+        this.equipmentService = equipmentService;
+    }
+
+
+    public Equipment convertToEntity(EquipmentAddDto equipmentAddDto) {
+
+        System.out.println("-----------------------Hello 1");
+
+        if (equipmentAddDto == null) {
+            System.out.println("-----------------------equipmentAddDto is null");
+        }
+
+        if (userservice == null) {
+            System.out.println("-----------------------User Service is null");
+
+        }
+
+        User user = userservice.findById(equipmentAddDto.getAdded_by_id());
+
+        System.out.println("-----------------------Hello 2");
+
+        Category category = categoryService.findById(equipmentAddDto.getCategory_Id());
+
+        System.out.println("-----------------------Hello 3");
+
+        // Set the data to equipment
+        Equipment equipment = new Equipment();
+        equipment.setName(equipmentAddDto.getName());
+        equipment.setPrice_per_day(equipmentAddDto.getPrice_per_day());
+        equipment.setAdded_by(user);
+        equipment.setCategory(category);
+
+        System.out.println("-----------------------Hello 4");
+
+        return equipment;
+
+    }
+
+    public Equipment convertToEntity(EquipmentUpdateDto equipmentUpdateDto, Long EquipmentId) {
+
+        Category category = categoryService.findById(equipmentUpdateDto.getCategory_Id());
+
+        // Set the data to equipment
+        Equipment equipment = equipmentService.findById(EquipmentId);
+        equipment.setName(equipmentUpdateDto.getName());
+        equipment.setPrice_per_day(equipmentUpdateDto.getPrice_per_day());
+        equipment.setCategory(category);
+
+        return equipment;
+
+    }
+}
