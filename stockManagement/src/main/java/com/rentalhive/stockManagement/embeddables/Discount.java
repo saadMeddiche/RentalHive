ebdd34d290c3cdecf9e40a8b400d0b138937f0f2
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-11-28 09:38:20 +0100

    Create Devis Entitie and Discound Embeddables class

---
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/embeddables/Discount.java
@@ -0,0 +1,56 @@
+package com.rentalhive.stockManagement.embeddables;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Embeddable
+public class Discount {
+
+    @Positive
+    @Max(100)
+    @Min(0)
+    @NotNull(message="The discount can not be null")
+    private Double percentage;
+
+    public Discount(){
+    }
+    public Discount(Double percentage){
+        this.percentage = percentage;
+    }
+
+
+    public Double getPercentage() {
+        return percentage;
+    }
+
+    public void setPercentage(Double percentage) {
+        this.percentage = percentage;
+    }
+
+
+
+    public String getDiscount(){
+
+        StringBuilder discount = new StringBuilder();
+
+        discount.append("-");
+        discount.append(this.percentage);
+        discount.append("%");
+
+        return discount.toString();
+    }
+
+//    public Double getDiscountPrefix(){
+//    }
+
+
+    @Override
+    public String toString() {
+        return "Discount{" +
+                "percentage=" + getDiscount() +
+                '}';
+    }
+}
