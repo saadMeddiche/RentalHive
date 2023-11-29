bd796990b8152c708bab1ec311f2d33497fb7a19
khaouti nouhaila <khaoutinouhaila@gmail.com>
2023-11-29 13:50:15 +0100

    resolve the problem of stackoverflow

--- stockManagement/src/main/java/com/rentalhive/stockManagement/controllers/DemandeController.java
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/controllers/DemandeController.java
@@ -17,7 +17,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Demande")
+@RequestMapping("/api")
 @AllArgsConstructor
 public class DemandeController extends ControllerHelper {
 
--- stockManagement/src/main/java/com/rentalhive/stockManagement/dto/DemandeDto/request/DemandeStockQuantityRequest.java
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/dto/DemandeDto/request/DemandeStockQuantityRequest.java
@@ -1,6 +1,5 @@
 package com.rentalhive.stockManagement.dto.DemandeDto.request;
 import com.rentalhive.stockManagement.dto.DemandeDto.StockQuantity;
-import com.rentalhive.stockManagement.DTO.DemandeDto.request.AddDemandeDto;
 import lombok.Data;
 
 import java.util.List;
