bfbf67632165b8ad1dd15d08085e15b899146f99
khaouti nouhaila <khaoutinouhaila@gmail.com>
2023-11-30 01:58:56 +0100

    fix some problems within the contract dto

--- stockManagement/src/main/java/com/rentalhive/stockManagement/dto/DevisDto.java
+++ stockManagement/src/main/java/com/rentalhive/stockManagement/dto/DevisDto.java
@@ -1,13 +1,9 @@
 package com.rentalhive.stockManagement.dto;
 
-import com.rentalhive.stockManagement.dto.DemandeDto.response.DemandeDto;
-import com.rentalhive.stockManagement.embeddables.Discount;
 import lombok.Data;
 
 
 @Data
 public class DevisDto {
-    private DemandeDto demandeDto;
-    private String pathPDF;
-    private Discount discount;
+    private Long id;
 }
